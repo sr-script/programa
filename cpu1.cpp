d2172b5aac34c47983c84ce639e48e621eb8e7eb
Daniel Vargas <83847722+sr-script@users.noreply.github.com>
2023-10-23 08:26:06 -0600

    Add files via upload

---
+++ cpu1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>  // Usar "cmath" en lugar de "math.h"
+
+void mostrarTiempo(clock_t tiempo);
+
+using namespace std;
+
+void mostrarTiempo(clock_t tiempo) {
+	tiempo = clock() - tiempo;
+	cout << endl << "Tiempo de ejecución es " << double(tiempo) / CLOCKS_PER_SEC << " seg." << endl;
+}
+
+int main() {
+	clock_t tiempo = clock();
+	long long i = 0;
+	double sum = 0;
+	
+	for (i = 1; i < 1000000000; i++) {  // Corregir la condición del bucle
+		sum = sum + log(double(i));
+	}
+	
+	mostrarTiempo(tiempo);
+	
+	cout << "Pulse para salir...";
+	cin.get();
+	
+	return 0;
+}
+
